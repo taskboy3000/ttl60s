d7fbe1706c53844c760c19510858b1a2cc560eac
Joe Johnston <jjohn@taskboy.com>
2018-06-02 07:44:56 -0400

    added various design docs
    This patch adds some rambling design docs.  I have figured out that
    the main game loop should be simulating something like hacking, in the
    same way that Funeral Quest simulates selling.

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 #   source ./env.sh
 
 export APP_HOME=`pwd`;
-export PERL5LIB=$APP_HOME/local/lib/perl5:$APP_HOME/local/lib/perl5/arm-linux-gnueabihf-thread-multi-64int:$APP_HOME/lib
+export PERL5LIB=$PERL5LIB:$APP_HOME/local/lib/perl5:$APP_HOME/local/lib/perl5/arm-linux-gnueabihf-thread-multi-64int:$APP_HOME/lib
 export PATH=$PATH:$APP_HOME/local/bin
 export MOJO_LISTEN='http://*:4000/'
 
